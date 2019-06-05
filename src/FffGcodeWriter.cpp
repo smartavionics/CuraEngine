9eec8897ba03401efecba3778f81ec8c1cf402ce
Mark Burton <markb@smartavionics.com>
2019-06-05 17:03:46 +0100

    Initial implementation of variable width spiralized walls.
    If a spiralized model layer is not solid, assume that the holes carve out the model to
    leave walls. The width of the walls is determined and the flow rate used when printing the
    spiral is modulated accordingly.
    
    The centre line of the wall also has to be adjusted to take into account the difference in
    wall width from the nominal value.

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -1594,8 +1594,52 @@ void FffGcodeWriter::processSpiralizedWall(const SliceDataStorage& storage, Laye
     const bool is_top_layer = ((size_t)layer_nr == (storage.spiralize_wall_outlines.size() - 1) || storage.spiralize_wall_outlines[layer_nr + 1] == nullptr);
     ConstPolygonRef wall_outline = part.insets[0][0]; // current layer outer wall outline
     const int seam_vertex_idx = storage.spiralize_seam_vertex_indices[layer_nr]; // use pre-computed seam vertex index for current layer
+
+    std::vector<float> flows(wall_outline.size(), 1.0); // flow multiplier for each line segment
+    std::vector<Point> shifts(wall_outline.size(), Point()); // position offset for each line segment
+
+    if (part.outline.size() > 1)
+    {
+        // part is not solid, wall line width can vary
+        const coord_t default_line_width = mesh.settings.get<coord_t>("wall_line_width_0");
+
+        ConstPolygonRef poly = part.insets[0][0];
+
+        Polygon inset_outline;
+
+        for (unsigned n = 0; n < poly.size(); ++n)
+        {
+            const Point& prev_point = poly[(n + poly.size() - 1) % poly.size()];
+            const Point& next_point = poly[(n + 1) % poly.size()];
+            const double corner_rads = LinearAlg2D::getAngleLeft(prev_point, poly[n], next_point);
+
+            Polygons lines;
+            Point bisector(poly[n] + rotate(normal(next_point - poly[n], default_line_width * 5), corner_rads / 2));
+            lines.addLine(poly[n], bisector);
+            lines = part.outline.intersectionPolyLines(lines);
+            if (lines.size() > 0)
+            {
+                // find clipped line segment that starts/ends close to poly[n]
+                unsigned ln = 0;
+                while (ln < (lines.size() - 1) && vSize2(lines[ln][0] - poly[n]) > 100 && vSize2(lines[ln][1] - poly[n]) > 100)
+                {
+                    ++ln;
+                }
+                Point clipped(lines[ln][(vSize2(lines[ln][0] - poly[n]) > 100) ? 0 : 1]);
+                coord_t line_width = vSize(lines[ln][1] - lines[ln][0]) * std::abs(std::sin(corner_rads / 2));
+#if 0
+                // diagnostic - print vertex bisector lines
+                gcode_layer.addTravel(poly[n]);
+                gcode_layer.addExtrusionMove(clipped, mesh_config.inset0_config, SpaceFillType::Lines, 0.1);
+#endif
+
+                flows[n] = std::max(std::min((double)line_width / default_line_width, 2.0), 0.5);
+                shifts[n] = normal(clipped - poly[n], default_line_width * (flows[n] - 1) / 2 / std::abs(std::sin(corner_rads / 2)));
+            }
+        }
+    }
     // output a wall slice that is interpolated between the last and current walls
-    gcode_layer.spiralizeWallSlice(mesh_config.inset0_config, wall_outline, ConstPolygonRef(*last_wall_outline), seam_vertex_idx, last_seam_vertex_idx, is_top_layer, is_bottom_layer);
+    gcode_layer.spiralizeWallSlice(mesh_config.inset0_config, wall_outline, ConstPolygonRef(*last_wall_outline), seam_vertex_idx, last_seam_vertex_idx, is_top_layer, is_bottom_layer, flows, shifts);
 }
 
 bool FffGcodeWriter::processInsets(const SliceDataStorage& storage, LayerPlan& gcode_layer, const SliceMeshStorage& mesh, const size_t extruder_nr, const PathConfigStorage::MeshPathConfigs& mesh_config, const SliceLayerPart& part) const
