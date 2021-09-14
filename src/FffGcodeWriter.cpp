04a2ac7bbd6b3f7d0dc98ddd04efe097973f607d
Mark Burton <burtoogle@gmail.com>
2021-09-14 09:22:53 +0100

    Make skin edge support compatible with gradual infill.
    Now appears to work with UM infill patterns but there's still problems with the mb-master
    infill patterns and gradual infill.

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -1722,45 +1722,47 @@ bool FffGcodeWriter::processSingleLayerInfill(const SliceDataStorage& storage, L
 
         if (hasSkinEdgeSupport)
         {
-            // infill region with skin above has to have at least one infill wall line
-            const size_t min_skin_below_wall_count = wall_line_count > 0 ? wall_line_count : 1;
-            const size_t skin_below_wall_count = density_idx == last_idx ? min_skin_below_wall_count : 0;
-            Infill infill_comp(pattern, zig_zaggify_infill, connect_polygons, infill_below_skin, outline_offset,
-                               infill_line_width, infill_line_distance_here, infill_overlap, infill_multiplier,
-                               infill_angle, gcode_layer.z / mesh.settings.get<Ratio>("infill_scaling_z"), infill_shift, max_resolution, max_deviation, skin_below_wall_count, infill_origin,
-                               perimeter_gaps, connected_zigzags, use_endpieces, skip_some_zags, zag_skip_count,
-                               pocket_size, pattern_resolution);
-            infill_comp.generate(infill_polygons_here, infill_lines_here, mesh.cross_fill_provider, &mesh);
-            // when both lines and polygons are created, convert the polygons to chains of lines so that they all get printed together
-            if (!infill_lines_here.empty() && !infill_polygons_here.empty())
+            Polygons infill_below_skin_per_density = infill_below_skin.intersection(in_outline);
+
+            if (!infill_below_skin_per_density.empty())
             {
-                for (ConstPolygonRef poly : infill_polygons_here)
+                // infill region with skin above has to have at least one infill wall line
+                const size_t min_skin_below_wall_count = wall_line_count > 0 ? wall_line_count : 1;
+                const size_t skin_below_wall_count = density_idx == last_idx ? min_skin_below_wall_count : 0;
+                Infill infill_comp(pattern, zig_zaggify_infill, connect_polygons, infill_below_skin_per_density, outline_offset,
+                                   infill_line_width, infill_line_distance_here, infill_overlap, infill_multiplier,
+                                   infill_angle, gcode_layer.z / mesh.settings.get<Ratio>("infill_scaling_z"), infill_shift, max_resolution, max_deviation, skin_below_wall_count, infill_origin,
+                                   perimeter_gaps, connected_zigzags, use_endpieces, skip_some_zags, zag_skip_count,
+                                   pocket_size, pattern_resolution);
+                infill_comp.generate(infill_polygons_here, infill_lines_here, mesh.cross_fill_provider, &mesh);
+                // when both lines and polygons are created, convert the polygons to chains of lines so that they all get printed together
+                if (!infill_lines_here.empty() && !infill_polygons_here.empty())
                 {
-                    for (unsigned n = 1; n < poly.size(); ++n)
+                    for (ConstPolygonRef poly : infill_polygons_here)
                     {
-                        infill_lines_here.addLine(poly[n-1], poly[n]);
+                        for (unsigned n = 1; n < poly.size(); ++n)
+                        {
+                            infill_lines_here.addLine(poly[n-1], poly[n]);
+                        }
+                        // stop the last line before it gets to the first point so that a chain is created rather than a loop
+                        const Point& last_point = poly[poly.size() - 1];
+                        infill_lines_here.addLine(last_point, last_point + normal(poly[0] - last_point, vSize(poly[0] - last_point) - 15));
                     }
-                    // stop the last line before it gets to the first point so that a chain is created rather than a loop
-                    const Point& last_point = poly[poly.size() - 1];
-                    infill_lines_here.addLine(last_point, last_point + normal(poly[0] - last_point, vSize(poly[0] - last_point) - 15));
+                    infill_polygons_here.clear();
                 }
+                if (density_idx < last_idx)
+                {
+                    const coord_t cut_offset =
+                        get_cut_offset(zig_zaggify_infill, infill_line_width, min_skin_below_wall_count);
+                    Polygons tool = infill_below_skin.offset(static_cast<int>(cut_offset));
+                    infill_lines_here.cut(tool);
+                }
+                infill_lines.add(infill_lines_here);
+                infill_polygons.add(infill_polygons_here);
+                infill_lines_here.clear();
                 infill_polygons_here.clear();
-            }
-            if (density_idx < last_idx)
-            {
-                const coord_t cut_offset =
-                    get_cut_offset(zig_zaggify_infill, infill_line_width, min_skin_below_wall_count);
-                Polygons tool = infill_below_skin.offset(static_cast<int>(cut_offset));
-                infill_lines_here.cut(tool);
-            }
-            infill_lines.add(infill_lines_here);
-            infill_polygons.add(infill_polygons_here);
-            infill_lines_here.clear();
-            infill_polygons_here.clear();
-            // normal processing for the infill that isn't below skin
-            in_outline = infill_not_below_skin;
-            if (density_idx == last_idx)
-            {
+                // normal processing for the infill that isn't below skin
+                in_outline = infill_not_below_skin.intersection(in_outline);
                 sparse_in_outline = infill_not_below_skin;
             }
         }
