db1fb74d41928d34d346bdd29b0eb3cac38eb90c
Mark Burton <burtoogle@gmail.com>
2022-08-31 23:13:31 +0100

    Increase the amount the skin between 1st and 2nd walls is grown.

--- src/WallsComputation.cpp
+++ src/WallsComputation.cpp
@@ -96,7 +96,7 @@ void WallsComputation::generateInsets(SliceLayerPart* part)
                 const Polygons outside_of_new_inset = part->insets[1].intersection(parts_above.offset(-line_width_0/2 - 10)).offset(line_width_x/2 + 10);
                 const Polygons inside_of_outer_wall = part->insets[0].offset(-line_width_0/2);
                 // we grow the skin so that it extends under the wall lines on the next layer
-                const coord_t grow_skin = line_width_x * 3 / 2;
+                const coord_t grow_skin = line_width_x * 2;
                 // if the skin between the outer wall and the new 2nd wall would be very thin, remove it by moving the 2nd wall back to hug the outer wall
                 const coord_t preshrink = mesh.settings.get<coord_t>("top_skin_preshrink") / 2;
                 Polygons skin = inside_of_outer_wall.difference(outside_of_new_inset).offset(grow_skin).intersection(inside_of_outer_wall).offset(-preshrink).offset(preshrink);
