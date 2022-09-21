20a0e3ef0a72ae7fd846fddbaaeb83dc53e3d93a
Mark Burton <burtoogle@gmail.com>
2022-09-21 09:31:14 +0100

    Use same last connector draw logic for Hilbert, honeycomb and gyroid patterns.

--- src/infill/HilbertInfill.cpp
+++ src/infill/HilbertInfill.cpp
@@ -147,6 +147,7 @@ void HilbertInfill::generateConnections(Polygons& result, const Polygons& outlin
         // we need to remember the first chain processed and the path to it from the first outline point
         // so that later we can possibly connect to it from the last chain processed
         unsigned first_chain_chain_index = std::numeric_limits<unsigned>::max();
+        unsigned first_chain_point_index = std::numeric_limits<unsigned>::max();
         std::vector<Point> path_to_first_chain;
 
         bool drawing = false; // true when a connector line is being (potentially) created
@@ -231,6 +232,7 @@ void HilbertInfill::generateConnections(Polygons& result, const Polygons& outlin
                 {
                     // this is the first chain to be processed, remember it
                     first_chain_chain_index = chain_index;
+                    first_chain_point_index = point_index;
                     path_to_first_chain.push_back(cur_point);
                 }
 
@@ -285,13 +287,14 @@ void HilbertInfill::generateConnections(Polygons& result, const Polygons& outlin
             }
         }
 
-        // we have now visited all the points in the outline, if a connector was (potentially) being drawn
-        // check whether the first chain is already connected to the last chain and, if not, draw the
-        // connector between
+        // we have now visited all the points in the outline, now check to see if a connector is to be drawn
+        // between the last chain and the first chain
+        // it is drawn if the first chain and the last chain are not the same chain and the first chain
+        // has not been connected to any another chain at the near end and it has not been connected to the last chain at the far end
         if (drawing && first_chain_chain_index != std::numeric_limits<unsigned>::max()
             && first_chain_chain_index != connector_start_chain_index
-            && connected_to[0][first_chain_chain_index] != connector_start_chain_index
-            && connected_to[1][first_chain_chain_index] != connector_start_chain_index)
+            && connected_to[first_chain_point_index][first_chain_chain_index] == std::numeric_limits<unsigned>::max()
+            && connected_to[(first_chain_point_index + 1) % 2][first_chain_chain_index] != connector_start_chain_index)
         {
             // output the connector line segments from the last chain to the first point in the outline
             connector_points.push_back(outline_poly[0]);
