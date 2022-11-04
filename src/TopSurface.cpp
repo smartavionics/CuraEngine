a21d8b2aa5c28c1df9745c666509ccaa254e2c20
Mark Burton <burtoogle@gmail.com>
2022-11-04 22:26:12 +0000

    Remove ironing lines shorter than 10 uM.

--- src/TopSurface.cpp
+++ src/TopSurface.cpp
@@ -102,6 +102,15 @@ bool TopSurface::ironing(const SliceMeshStorage& mesh, const GCodePathConfig& li
     Polygons ironing_lines;
     infill_generator.generate(ironing_polygons, ironing_lines);
 
+    // remove lines shorter than 10 uM
+    for (auto it = ironing_lines.begin(); it < ironing_lines.end(); ++it)
+    {
+        if (vSize2((*it)[0] - (*it)[1]) < 100)
+        {
+            ironing_lines.erase(it, it + 1);
+        }
+    }
+
     if (ironing_polygons.empty() && ironing_lines.empty())
     {
         return false; //Nothing to do.
