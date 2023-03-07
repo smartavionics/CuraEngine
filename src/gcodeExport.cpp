fda956ae2f62a58446a5fd04b5d62e7d43a7bcf2
Mark Burton <burtoogle@gmail.com>
2023-03-07 08:53:57 +0000

    Now outputs FILAMENT_USED comment before each layer to report total filament used so far.

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -572,6 +572,24 @@ void GCodeExport::writeTypeComment(const PrintFeatureType& type)
 
 void GCodeExport::writeLayerComment(const LayerIndex layer_nr)
 {
+    const Scene& scene = Application::getInstance().current_slice->scene;
+    *output_stream << ";FILAMENT_USED: ";
+    for (size_t i = 0; i < scene.extruders.size(); i++)
+    {
+        if (i > 0)
+        {
+            *output_stream << ", ";
+        }
+        if (flavor != EGCodeFlavor::MARLIN_VOLUMATRIC)
+        {
+            *output_stream << getTotalFilamentUsed(i) / (1000 * extruder_attr[i].filament_area) << "m";
+        }
+        else //Use volumetric filament used.
+        {
+            *output_stream << getTotalFilamentUsed(i) << "mm3";
+        }
+    }
+    *output_stream << new_line;
     *output_stream << ";LAYER:" << layer_nr << new_line;
 }
 
