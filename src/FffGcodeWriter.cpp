cb979090eedbd5c100d245335537bc2e8ce6ac11
Mark Burton <burtoogle@gmail.com>
2024-08-16 09:50:29 +0100

    Use a minimum value of 0.01mm for bridge_skin_expansion.
    This ensures that bridge skins will overlap (by a tiny amount) walls below so the skin
    edge will be considered to be supported and this affects the choice of skin line angle.

--- src/FffGcodeWriter.cpp
+++ src/FffGcodeWriter.cpp
@@ -2855,7 +2855,7 @@ void FffGcodeWriter::processTopBottomWithBridges(const SliceDataStorage& storage
         // determine the bridge skin regions
 
         // apply bridge_skin_overlap_mm
-        const coord_t bridge_skin_expansion = mesh.settings.get<coord_t>((n > 1) ? "bridge_skin_overlap_mm_3" : (n > 0) ? "bridge_skin_overlap_mm_2" : "bridge_skin_overlap_mm");
+        const coord_t bridge_skin_expansion = std::max(MM2INT(0.01), mesh.settings.get<coord_t>((n > 1) ? "bridge_skin_overlap_mm_3" : (n > 0) ? "bridge_skin_overlap_mm_2" : "bridge_skin_overlap_mm"));
 
         Polygons bridge_skin = bridge_regions[n].intersection(skin_part.outline).offset(std::min(max_bridge_skin_expansion, bridge_skin_expansion));
 
