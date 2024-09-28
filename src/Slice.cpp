e7900d48967d62a3136f8fb30c938d8da56b7912
Mark Burton <burtoogle@gmail.com>
2024-09-28 20:43:34 +0100

    Implement mesh_priority for one-at-a-time mode.

--- src/Slice.cpp
+++ src/Slice.cpp
@@ -15,8 +15,40 @@ Slice::Slice(const size_t num_mesh_groups)
 void Slice::compute()
 {
     logWarning("%s", scene.getAllSettingsString().c_str());
+
+    std::vector<std::pair<int,std::vector<MeshGroup>::iterator>> prioritised_meshes;
+
+    int max_priority = 0;
     for (std::vector<MeshGroup>::iterator mesh_group = scene.mesh_groups.begin(); mesh_group != scene.mesh_groups.end(); mesh_group++)
     {
+        int priority = 0;
+        for (Mesh& mesh : mesh_group->meshes)
+        {
+            int pri = mesh.settings.get<int>("mesh_priority");
+            if (pri > priority)
+            {
+                priority = pri;
+            }
+        }
+        prioritised_meshes.push_back(std::make_pair(priority, mesh_group));
+        if (priority > max_priority)
+        {
+            max_priority = priority;
+        }
+    }
+
+    if (max_priority > 0)
+    {
+        auto sort_by_increasing_priority = [] (std::pair<int,std::vector<MeshGroup>::iterator>& a, std::pair<int,std::vector<MeshGroup>::iterator>& b)
+        {
+            return a.first > b.first;
+        };
+        std::sort(prioritised_meshes.begin(), prioritised_meshes.end(), sort_by_increasing_priority);
+    }
+
+    for (unsigned n = 0; n < prioritised_meshes.size(); ++n)
+    {
+        std::vector<MeshGroup>::iterator mesh_group = prioritised_meshes[n].second;
         scene.current_mesh_group = mesh_group;
         for (ExtruderTrain& extruder : scene.extruders)
         {
